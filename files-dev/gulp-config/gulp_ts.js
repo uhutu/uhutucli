a4da6e2fc77741ba0c2ca6898ca6f661fae06d6e
srnpr <srnpr@macpro>
2017-03-28 17:49:00 +0800

    a

---
+++ files-dev/gulp-config/gulp_ts.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var tsGulp = require("gulp-typescript");
+var tsProject = tsGulp.createProject("../../tsconfig.json");
+
+
+gulp.task("ts", function () {
+
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest(tsProject.options.outDir));
+});
