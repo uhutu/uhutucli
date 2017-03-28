e0432a10228048778b7edf2a89537644ad837d3c
srnprmini <srnpr@srnprminideMac-mini.local>
2017-03-28 22:27:57 +0800

    a

--- cli-local/aim-interface/aim-local.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MAimLocalEnv = (function () {
-    function MAimLocalEnv() {
-    }
-    return MAimLocalEnv;
-}());
-exports.MAimLocalEnv = MAimLocalEnv;
