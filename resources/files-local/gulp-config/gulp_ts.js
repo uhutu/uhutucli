7b4bb39b2b629ace754945db50eb4e7ef5986b7f
srnpr <srnpr@macpro>
2017-03-31 10:57:50 +0800

    修改dev-watch为新版

--- resources/files-local/gulp-config/gulp_ts.js
+++ resources/files-local/gulp-config/gulp_ts.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var tsGulp = require("gulp-typescript");
 var tsProject = tsGulp.createProject("../../../tsconfig.json");
+var watch=require('gulp-watch');
 
 
 gulp.task("ts", function () {
@@ -20,6 +21,14 @@ gulp.task("watch:ts", function () {
 });
 gulp.task("watch", function () {
 
-    gulp.watch(["../../../src/**/*.ts"], ['watch:ts']);
+    //gulp.watch(["../../../src/**/*.ts"], ['watch:ts']);
+   
 
+       return watch("../../../src/**/*.ts",function(){
+           tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest(tsProject.options.outDir)).pipe(gulp.dest(
+            "/usr/local/lib/node_modules/uhutu-cli/compile/"
+        ));
+       })
 });
