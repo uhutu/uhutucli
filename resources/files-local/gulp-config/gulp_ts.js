974c2ac6d093f872fcf98d6851aea958fdc71646
srnprmini <srnpr@srnprminideMac-mini.local>
2017-04-03 17:23:45 +0800

    调整dev命令 去掉install 改由gulp拷贝文件

--- resources/files-local/gulp-config/gulp_ts.js
+++ resources/files-local/gulp-config/gulp_ts.js
@@ -1,30 +1,33 @@
 var gulp = require('gulp');
 var tsGulp = require("gulp-typescript");
-var tsProject = tsGulp.createProject("../../../tsconfig.json");
+//var tsProject = tsGulp.createProject("../../../tsconfig.json");
 var watch=require('gulp-watch');
 
+var sTargetDept="/usr/local/lib/node_modules/uhutu-cli/";
 
-gulp.task("ts", function () {
 
+gulp.task("ts:compile", function () {
+    var tsProject = tsGulp.createProject("../../../tsconfig.json");
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest(tsProject.options.outDir));
 });
 
 
-gulp.task("watch:ts", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest(tsProject.options.outDir)).pipe(gulp.dest(
-            "/usr/local/lib/node_modules/uhutu-cli/compile/"
-        ));
+gulp.task("ts:copy", function () {
+    return gulp.src(['../../../**/*','!../../../node_modules/**/*','!../../../.git/**/*']).pipe(
+        gulp.dest(sTargetDept)
+    );
 });
-gulp.task("watch", function () {
 
-    //gulp.watch(["../../../src/**/*.ts"], ['watch:ts']);
-   
 
-       return watch("../../../src/**/*.ts",function(){
+
+gulp.task("ts",["ts:compile","ts:copy"]),
+
+
+gulp.task("watch:ts", function () {
+    return watch("../../../src/**/*.ts", { ignoreInitial: false },function(){
+           var tsProject = tsGulp.createProject("../../../tsconfig.json");
            tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest(tsProject.options.outDir)).pipe(gulp.dest(
@@ -32,5 +35,9 @@ gulp.task("watch", function () {
         ));
        })
 });
+gulp.task("watch:resources", function () {
+    return watch("../../../resources/**/*", { ignoreInitial: false }).pipe(gulp.dest(sTargetDept+"resources/"));
+});
+gulp.task("watch", ["watch:ts","watch:resources"]);
 
 
--- webdoc/npm_publish.md
+++ webdoc/npm_publish.md
@@ -4,4 +4,4 @@ npm version 1.0.x
 git push --follow-tags
 
 https://circleci.com
-使用circleci的服务进行发布npm。
+使用circle的服务进行发布npm。
