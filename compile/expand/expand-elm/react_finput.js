8bd91677377e4371263337352b443b23b1eb382a
srnpr <srnpr@macpro>
2017-04-13 16:35:40 +0800

    add datepicker

---
+++ compile/expand/expand-elm/react_finput.js
@@ -0,0 +1,16 @@
+"use strict";
+var processItem = require("../../project/support-operate/item_support");
+var MexpandReactUicon = (function () {
+    function MexpandReactUicon() {
+    }
+    MexpandReactUicon.prototype.expandOpen = function (oItem) {
+        processItem.checkPropWithQuotes(oItem, "placeholder", "placeholder");
+        processItem.checkPropWithEmpty(oItem, "secure", "secureTextEntry");
+        processItem.checkPropWithQuotes(oItem, "keyboard", "keyboardType");
+        processItem.checkPropWithEmpty(oItem, "multiline", "multiline");
+        oItem.targetAttr.set('underlineColorAndroid', '"transparent"');
+        return oItem;
+    };
+    return MexpandReactUicon;
+}());
+module.exports = new MexpandReactUicon();
